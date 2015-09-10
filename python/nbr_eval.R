63f311807ed5272a2b2b38095137ccfcbb6362fc
Hongjian <hongjian-wang@outlook.com>
2015-09-10 11:20:00 -0400

    negative binomial regression with R

--- python/nbr_eval.R
+++ python/nbr_eval.R
@@ -13,8 +13,11 @@ for (i in 1:77) {
 	mod <- glm.nb( data=dat )
 	ybar <- predict(mod, newdata=f[i,], type=c('response') )
 	
+	print(paste(y[i,], ybar))
 	errors = c(errors, abs(ybar - y[i,]) )
 }
+
+print(paste("MAE", mean(errors), "SD", sd(errors), "MRE", mean(errors) / mean(y$V1)))
 
 
 
