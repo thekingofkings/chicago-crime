6cc114b70131fc943478bccb54d68b6c83643f89
Hongjian <hongjian-wang@outlook.com>
2016-02-17 13:12:01 -0500

    fix Chicago CA plot ID bug

--- R/shpFilePlot.R
+++ R/shpFilePlot.R
@@ -2,10 +2,12 @@
 library(maptools)
 
 CAs = readShapeSpatial('../data/ChiCA_gps/ChiCaGPS')
+ids = as.numeric(as.character(CAs$AREA_NUMBE))
+
 
 pdf('id-graph.pdf')
-plot(CAs, border='blue')
+plot(CAs, border='darkgreen')
 cr = coordinates(CAs)
-#text(cr, labels=as.character(1:77))
-points(cr)
+text(cr, labels=ids)
+#points(cr)
 dev.off()
