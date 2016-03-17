330cce4d694e78e332f17eea9bbc04558115e146
Hongjian <hongjian-wang@outlook.com>
2016-03-17 13:22:34 -0400

    R-script use dot identifier in formula
    http://stackoverflow.com/a/5251676/1287369

--- R/pvalue-evaluation.R
+++ R/pvalue-evaluation.R
@@ -26,7 +26,7 @@ spatialWeight <- function( ca, leaveOneOut = -1 ) {
 
 
 
-leaveOneOut <- function(demos, ca, w2, Y, coeff=FALSE, normalize=FALSE, socialnorm="bydestination", exposure="exposure") {
+leaveOneOut <- function(demos, ca, w2, Y, coeff=FALSE, normalize=FALSE, socialnorm="bydestination", exposure="exposure", sociallag=TRUE, spatiallag=TRUE) {
     N <- length(Y)
     # leave one out evaluation
     errors = c()
@@ -37,43 +37,49 @@ leaveOneOut <- function(demos, ca, w2, Y, coeff=FALSE, normalize=FALSE, socialno
     w1 <- spatialWeight(ca)
     for ( i in 1:N ) {
         F <- demos[-i, , drop=FALSE]
-        spt <- spatialWeight(ca, i)
-        sco <- w2[-i, -i]
-
-        if (socialnorm == "bysource") {
-            cs <- colSums(sco)
-            sco <- sweep(sco, 2, cs, "/")
-        } else if (socialnorm == "bydestination") {
-            rs <- rowSums(sco)
-            sco <- sweep(sco, 1, rs, "/")
-        } else if (socialnorm == 'bypair') {
-            sco <- sco + t(sco)
-            s <- sum(sco)
-            sco <- sco / s
-            stopifnot( sco[4, 4] == 0, nrow(sco)==N-1, ncol(sco)==N-1, sum(sco) == 1)
-        }
-            
         y <- Y[-i]
+        
+        if (sociallag) {
+            # training set
+            sco <- w2[-i, -i]
 
-        F[,'spatial.lag'] = as.vector(spt %*% y)
-        F[,'social.lag'] = as.vector(sco %*% y)
+            if (socialnorm == "bysource") {
+                cs <- colSums(sco)
+                sco <- sweep(sco, 2, cs, "/")
+            } else if (socialnorm == "bydestination") {
+                rs <- rowSums(sco)
+                sco <- sweep(sco, 1, rs, "/")
+            } else if (socialnorm == 'bypair') {
+                sco <- sco + t(sco)
+                s <- sum(sco)
+                sco <- sco / s
+                stopifnot( sco[4, 4] == 0, nrow(sco)==N-1, ncol(sco)==N-1, sum(sco) == 1)
+            }
+
+            F[,'social.lag'] = as.vector(sco %*% y)
+        }
+
+        if (spatiallag) {
+            spt <- spatialWeight(ca, i)
+            F[,'spatial.lag'] = as.vector(spt %*% y)
+        }
+       
+       
 
         # normalize features
-		if (normalize) {
-			F <- scale(F, center=TRUE, scale=TRUE)
-			F.center <- as.vector(attributes(F)["scaled:center"][[1]])
-			F.scale <- as.vector(attributes(F)["scaled:scale"][[1]])
-		}
+        if (normalize) {
+            F <- scale(F, center=TRUE, scale=TRUE)
+            F.center <- as.vector(attributes(F)["scaled:center"][[1]])
+            F.scale <- as.vector(attributes(F)["scaled:scale"][[1]])
+        }
         # fit NB model
         dat <- data.frame(y, F)
         stopifnot( all(is.finite(as.matrix(dat))) )
         mod <- tryCatch( {
             if (exposure == "exposure") {
-                mod <- glmmadmb(y ~ poverty.index + residential.stability + ethnic.diversity + spatial.lag +
-                                social.lag + offset(total.population), data=dat, family="nbinom", verbose=FALSE)
+                mod <- glmmadmb(y ~ offset(total.population) + ., data=dat, family="nbinom", verbose=FALSE)
             } else {
-                mod <- glmmadmb(y ~ poverty.index + residential.stability + ethnic.diversity + spatial.lag +
-                                social.lag + total.population, data=dat, family="nbinom", verbose=FALSE)
+                mod <- glmmadmb(y ~ ., data=dat, family="nbinom", verbose=FALSE)
             }
             mod
         }, error=function(e) e)
@@ -162,22 +168,20 @@ leaveOneOut.PermuteLag <- function(demos, ca, w2, Y, normalize=FALSE, socialnorm
             }
 
             # normalize features
-			if (normalize) {
-				F <- scale(F, center=TRUE, scale=TRUE)
-				F.center <- as.vector(attributes(F)["scaled:center"][[1]])
-				F.scale <- as.vector(attributes(F)["scaled:scale"][[1]])
-			}
+            if (normalize) {
+                F <- scale(F, center=TRUE, scale=TRUE)
+                F.center <- as.vector(attributes(F)["scaled:center"][[1]])
+                F.scale <- as.vector(attributes(F)["scaled:scale"][[1]])
+            }
             # fit NB model
             dat <- data.frame(Y[-i], F)
             names(dat)[1] <- "y"
 
             mod <- tryCatch( {
                 if (exposure == "exposure") {
-                    mod <- glmmadmb(y ~ poverty.index + residential.stability + ethnic.diversity + spatial.lag +
-                                    social.lag + offset(total.population), data=dat, family="nbinom", verbose=FALSE)
+                    mod <- glmmadmb(y ~ offset(total.population) + ., data=dat, family="nbinom", verbose=FALSE)
                 } else {
-                    mod <- glmmadmb(y ~ poverty.index + residential.stability + ethnic.diversity + spatial.lag +
-                                    social.lag + total.population, data=dat, family="nbinom", verbose=FALSE)
+                    mod <- glmmadmb(y ~ ., data=dat, family="nbinom", verbose=FALSE)
                 }
                 mod
             }, error=function(e) e)
@@ -202,9 +206,11 @@ leaveOneOut.PermuteLag <- function(demos, ca, w2, Y, normalize=FALSE, socialnorm
             }
             stopifnot( length(social.lag) == 1)
             dn <- data.frame(demos[i, , drop=FALSE], spatial.lag, social.lag)
-			if (normalize) {
-				dn <- data.frame(scale(dn, center=F.center, scale=F.scale))
-			}
+
+            if (normalize) {
+                dn <- data.frame(scale(dn, center=F.center, scale=F.scale))
+            }
+            
             ybar <- predict(mod, newdata=dn, type=c('response'))
             errors <- c(errors, abs(ybar - Y[i]))
         }
@@ -222,7 +228,8 @@ ca = readShapeSpatial("../data/ChiCA_gps/ChiCaGPS")
 w1 <- spatialWeight(ca)
 
 demos <- read.table('pvalue-demo.csv', header=TRUE, sep=",")
-focusColumn <- names(demos) %in% c("total.population", "poverty.index", "residential.stability",
+focusColumn <- names(demos) %in% c("total.population", "population.density",
+                                   "poverty.index", "residential.stability",
                                    "ethnic.diversity")
 demos.part <- demos[focusColumn]
 cat("Selected Demographics features:\n", names(demos.part), "\n")
