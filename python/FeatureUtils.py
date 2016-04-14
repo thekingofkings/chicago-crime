Fix the social lag normalization by source (Issue #8)

python/FeatureUtils.py
@@ -181,17 +181,21 @@ def generate_transition_SocialLag(year = 2010, lehd_type=0, region='ca', leaveOu
             W[ordkey.index(srcid)] = np.zeros( (1,len(ts)) )
             
     
-    W = np.transpose(W)
+    
     # update diagonal as 0
     for i in range(len(W)):
         W[i,i] = 0
     # first make all self-factor 0
+    assert W.dtype == "float64"
         
     # normalization section
     if normalization == 'source':
-        sW = np.sum(W, axis=0)
+        sW = np.sum(W, axis=1, keepdims=True)
         W = W / sW
+        assert abs( np.sum(W[1,]) - 1 ) < 0.0000000001 and W.dtype == "float64"
     elif normalization == 'destination':
+        # use in-flow (therefore transpose)
+        W = np.transpose(W)
         sW = np.sum(W, axis=1)
         sW = sW.reshape((len(sW),1))
         W = W / sW
@@ -393,7 +397,7 @@ if __name__ == '__main__':
     
     
     t = generate_transition_SocialLag(year = 2010, lehd_type=0, region='ca', 
-                                      leaveOut=-1, normalization='pair')
+                                      leaveOut=-1, normalization='source')
 #    generateDotFile(t, 0.08, 'sociallag')
     
 #    h = retrieve_health_data()

python/taxiFlow.py
@@ -38,25 +38,31 @@ def getTaxiFlow(leaveOut = -1, normalization="bydestination", gridLevel='ca'):
     elif gridLevel == 'tract':
         s = np.loadtxt("TF_tract.csv", delimiter=",")
     n = s.shape[0]
-    s = np.transpose(s)
+    
     for i in range(n):
         s[i,i] = 0
-        
-    if normalization == 'bydestination':
-        fsum = np.sum(s, axis=1, keepdims=True)
-        assert fsum.shape == (n,1)
-        s = s / fsum
-        assert s.sum() == 77 and s.sum(axis=1)[9] - 1 <= 0.000000002
-    elif normalization == 'bysource':
-        fsum = np.sum(s, axis=0)
-        s = s / fsum
-        assert s.sum() == 77 and s.sum(axis=0)[23] - 1 <= 0.000000002
-    elif normalization == 'none':
-        pass
     
     if leaveOut > 0:
         s = np.delete(s, leaveOut -1, 0)
         s = np.delete(s, leaveOut -1, 1)
+        
+    n = s.shape[0]
+        
+    assert s.dtype == "float64"
+    if normalization == 'bydestination':
+        s = np.transpose(s)
+        fsum = np.sum(s, axis=1, keepdims=True)
+        assert fsum.shape == (n,1)
+        s = s / fsum
+        assert s.sum() == n and abs(s.sum(axis=1)[9] - 1) <= 0.000000001
+    elif normalization == 'bysource':
+        fsum = np.sum(s, axis=1, keepdims=True)
+        s = s / fsum
+        assert s.sum() == n and abs(s.sum(axis=0)[23] - 1) <= 0.000000001
+    elif normalization == 'none':
+        pass
+    
+    
     return s
 
 
@@ -117,4 +123,4 @@ def generateTaxiFlow(gridLevel='ca'):
 if __name__ == '__main__':
     
 #    generateTaxiFlow()
-    s = getTaxiFlow(normalization="bydestination")
+    s = getTaxiFlow(leaveOut=2, normalization="bydestination")