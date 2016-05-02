4bef070f55cb0d9ff6763cfb567c77c1050c589c
Hongjian <hongjian-wang@outlook.com>
2016-05-01 23:07:54 -0400

    remove itermediate output in R script

--- R/NBUtils.R
+++ R/NBUtils.R
@@ -170,7 +170,6 @@ leaveOneOut <- function(demos, ca, w2, Y, coeff=FALSE, normalize=FALSE, socialno
         dat <- data.frame(Y[-i], F)
         colnames(dat)[1] <- "y"
 
-        cat(as.matrix(dat), "\n")
         stopifnot( all(is.finite(as.matrix(dat))) )
 
         mod <- learnNB(dat, exposure)
