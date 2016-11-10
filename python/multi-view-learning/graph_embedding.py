e8a93fc77a946242df07cfd4562dca6b89a7a2cb
Hongjian <hongjian-wang@outlook.com>
2016-11-10 17:57:44 -0500

    Fix taxi flow embedding ID does not match bug (#30)

--- python/multi-view-learning/graph_embedding.py
+++ python/multi-view-learning/graph_embedding.py
@@ -1,4 +1,13 @@
- 
+"""
+Hongjian
+
+Problem: use taxi flow as an independent view to predict crime.
+Task:
+    1. Get graph embedding representation of regions from taxi flow.
+    2. Use leaveOneOut to test the error of this single view.
+    3. Return a ML model on this single view, given training region ID.
+"""
+
 import numpy as np
 import statsmodels.api as sm
 from sklearn.cross_validation import LeaveOneOut
@@ -11,7 +20,7 @@ from Crime import Tract
 
 
 
-def get_graph_embedding_features(hasConstant=True):
+def get_graph_embedding_features():
     """
     Get graph embedding vector, which is generated from LINE
     """
@@ -22,15 +31,11 @@ def get_graph_embedding_features(hasConstant=True):
             ls = line.strip().split(" ")
             ge.append([float(i) for i in ls])
     ge = np.array(ge)
-    if hasConstant:
-        ge[:,0] = 1
-        return ge
-    else:
-        return ge[:, 1:]
+    ge = ge[np.argsort(ge[:,0])]
+    return ge[:,1:]
+
     
     
-
-
 def leaveOneOut_error(Y, X):
     """
     Use GLM model from python statsmodels library to fit data.
@@ -46,21 +51,20 @@ def leaveOneOut_error(Y, X):
     errors = []
     errs_train = np.zeros(2)
     loo = LeaveOneOut(len(Y))
-    for train_idx, test_idx in loo:
-        X_train, X_test = X[train_idx], X[test_idx]
-        Y_train, Y_test = Y[train_idx], Y[test_idx]
+    X = sm.add_constant(X, prepend=False)
+    for train_idx, test_idx in loo:    
+        X_train, Y_train = X[train_idx], Y[train_idx]
         # Train NegativeBinomial Model from statsmodels library
-        nbm = sm.GLM(Y_train, X_train, family=sm.families.NegativeBinomial())
-        nb_res = nbm.fit()
-        ybar = nbm.predict(nb_res.params, X_train)
-        er_train = np.mean(np.abs(ybar - Y_test))
-        errs_train += er_train, er_train / np.mean(Y_test)
-#        print er_train, er_train / np.mean(Y_test)
-        
-        ybar = nbm.predict(nb_res.params, X_test)
-        errors.append(np.abs(ybar - Y_test))
+        glm = sm.GLM(Y_train, X_train, family=sm.families.NegativeBinomial())
+        nbm = glm.fit()
+        ybar = nbm.predict(X[train_idx])
+        er_train = np.mean(np.abs(ybar - Y[train_idx]))
+        errs_train += er_train, er_train / np.mean(Y[train_idx])
+#        print er_train, er_train / np.mean(Y[train_idx])
+        ybar = nbm.predict(X[test_idx])
+        errors.append(np.abs(ybar - Y[test_idx]))
     print errs_train / len(Y)
-    return np.mean(errors), np.mean(errors) / np.mean(Y)
+    return np.mean(errors), np.mean(errors / Y), np.mean(errors) / np.mean(Y)
     
     
     
